a9e1b7bd74abdb21519ee79b2aa20b249e86083b
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-07-16 00:58:07 +0530

    bug fix

--- src/pages/Labs.jsx
+++ src/pages/Labs.jsx
@@ -4,7 +4,7 @@ const Labs = () => {
   return (
     <div className="card mt-4">
       <div className="card-body text-center">
-        <h2 className="card-title">Welcome to Labs!</h2>
+        <h2 className="card-title">Welcome to Labs</h2>
         <p className="card-text">
           We are glad to have you here. Explore the app and enjoy our features.
         </p>
